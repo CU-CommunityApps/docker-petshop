78fcd8dccf055196797ba7aa2f84a8e68d5de829
Paul Allen <pea1@cornell.edu>
2017-01-10 14:41:13 -0500

    revise script to be aware if it is running within Jenkins

--- build-scripts/docker-build.sh
+++ build-scripts/docker-build.sh
@@ -7,12 +7,16 @@
 echo BUILD_NUMBER: $BUILD_NUMBER
 echo DOCKER_ENV: $DOCKER_ENV
 
-#rm -rf keys || echo "keys not present"
-#mkdir keys || echo "keys already exist"
-#cp /var/jenkins_home/keys/id_rsa* keys/
-#cp ${GIT_DEPLOY_KEY} keys/id_rsa
-#cp ${EYAML_PRIVATE_KEY} keys/
-#cp ${EYAML_PUBLIC_KEY} keys/
+
+if [ -n "$JENKINS_HOME" ]; then
+  # Do Jenkins-specific actions
+  rm -rf keys || echo "keys not present"
+  mkdir keys || echo "keys already exist"
+  cp $JENKINS_HOME/keys/id_rsa* keys/
+  #cp ${GIT_DEPLOY_KEY} keys/id_rsa
+  #cp ${EYAML_PRIVATE_KEY} keys/
+  #cp ${EYAML_PUBLIC_KEY} keys/
+fi
 
 echo $BUILD_NUMBER > version
 
@@ -22,4 +26,7 @@ docker tag dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:latest dtr.cucloud.net/cs/pe
 docker push dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:v_${BUILD_NUMBER}
 docker rmi dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:v_${BUILD_NUMBER}
 #docker images | grep petshop
-#rm -rf keys || echo "keys have been deleted"
+
+if [ -n "$JENKINS_HOME" ]; then
+  rm -rf keys || echo "keys have been deleted"
+fi
