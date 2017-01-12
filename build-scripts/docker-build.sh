9b130a3110e6cef6699886c19be46229ab68d515
Paul Allen <pea1@cornell.edu>
2017-01-12 12:15:03 -0500

    add build-args to script

--- build-scripts/docker-build.sh
+++ build-scripts/docker-build.sh
@@ -3,10 +3,11 @@
 # Set value for varaibles, if they don't exist.
 : ${DOCKER_ENV:=local}
 : ${BUILD_NUMBER:=1}
+: ${APP_NAME:=petshop}
 
-echo BUILD_NUMBER: $BUILD_NUMBER
 echo DOCKER_ENV: $DOCKER_ENV
-
+echo BUILD_NUMBER: $BUILD_NUMBER
+echo APP_NAME: $APP_NAME
 
 if [ -n "$GIT_DEPLOY_KEY_FILE" ]; then
   # Do Jenkins-specific actions
@@ -20,11 +21,15 @@ fi
 
 echo $BUILD_NUMBER > version
 
-docker build --build-arg DOCKER_ENV=${DOCKER_ENV} -t dtr.cucloud.net/cs/petshop-${DOCKER_ENV} .
-docker push dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:latest
-docker tag dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:latest dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:v_${BUILD_NUMBER}
-docker push dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:v_${BUILD_NUMBER}
-docker rmi dtr.cucloud.net/cs/petshop-${DOCKER_ENV}:v_${BUILD_NUMBER}
+docker build \
+  --build-arg DOCKER_ENV=${DOCKER_ENV} \
+  --build-arg BUILD_NUMBER=${BUILD_NUMBER} \
+  --build-arg APP_NAME=${APP_NAME} \
+  -t dtr.cucloud.net/cs/${APP_NAME}-${DOCKER_ENV} .
+docker push dtr.cucloud.net/cs/${APP_NAME}-${DOCKER_ENV}:latest
+docker tag dtr.cucloud.net/cs/${APP_NAME}-${DOCKER_ENV}:latest dtr.cucloud.net/cs/${APP_NAME}-${DOCKER_ENV}:v_${BUILD_NUMBER}
+docker push dtr.cucloud.net/cs/${APP_NAME}-${DOCKER_ENV}:v_${BUILD_NUMBER}
+docker rmi dtr.cucloud.net/cs/${APP_NAME}-${DOCKER_ENV}:v_${BUILD_NUMBER}
 #docker images | grep petshop
 
 if [ -n "$GIT_DEPLOY_KEY_FILE" ]; then
