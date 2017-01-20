96ac0743553b254feaa1c163a2e57ba67aa3deca
Paul Allen <pea1@cornell.edu>
2017-01-20 09:00:19 -0500

    improve commentds

--- container-scripts/launch.sh
+++ container-scripts/launch.sh
@@ -3,7 +3,8 @@
 # This script does final setup of the container
 # and launches the main container process.
 
-# Descrypt secrets. The /tmp/secrets directory is populated
+#### Decrypt secrets manually, without puppet. ####
+# The /tmp/secrets directory is populated
 # by the puppet configuration
 
 # This will decrypt into /tmp/secrets/service.conf
@@ -14,6 +15,8 @@ chmod 0400 /tmp/secrets/service.conf
 chown root:root /tmp/secrets/service.conf
 
 #### Run Puppet at container launch. ####
+# An alternative way to decrypt secrets at container launch,
+# rather than what's above.
 puppet apply --verbose --modulepath=/modules \
   --hiera_config=/modules/petshop/hiera.yaml \
   --environment=local -e "class { 'petshop::launch': }"
